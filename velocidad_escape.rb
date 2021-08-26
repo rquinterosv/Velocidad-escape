dc73c6e6697326d336b51f16ffc4e7318d935222
Rafael Quinteros <rafael.quinterosvq@gmail.com>
2021-08-26 00:04:03 -0400

    definitivo

--- velocidad_escape.rb
+++ velocidad_escape.rb
@@ -1,4 +1,10 @@
 g = ARGV[0]
 r = ARGV[1]
 
-asdasdas
+puts "Los numeros seleccionados son #{g} y #{r}"
+
+multiplicacion = g.to_f * (r.to_f * 1000) * 2
+
+velocidad_escape = Math.sqrt(multiplicacion.to_f)
+
+puts "La velocidad de escape deberia se = #{velocidad_escape}"
